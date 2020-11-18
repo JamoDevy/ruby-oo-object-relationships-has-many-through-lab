23344e48b2172955ba16d21da2135a1e61dac46e
devin <devinlclarke@gmail.com>
2020-11-18 15:12:24 -0500

    commit tree d68cc1be06c83020a1751b74dc77a003de165a3a

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
+class Doctor
+    @@all=[]
+  
+    def self.all
+      @@all
+    end
+  
+    attr_accessor :name, :appointments, :patient
+  
+    def initialize(name)
+      @name = name
+      @appointments = []
+      @@all << self
+    end
+  
+    def new_appointment(date, patient)
+       appointment = Appointment.new(patient, date, self)
+       @appointments << appointment
+       
+    end
+  
+    def patients
+      @appointments.collect do |appointment|
+        appointment.patient
+      end
+    end
 
+    
+  end
